6a1a3b07a918d1b47ea93e66fc9e1338d5caf9a3
Said <alievsaid0909@yandex.ru>
2026-03-03 10:52:59 +0300

    chore(api-contracts): добавление react-query и конфигурация orval

--- packages/api-contracts/orval.config.ts
+++ packages/api-contracts/orval.config.ts
@@ -6,9 +6,24 @@ export default defineConfig({
     output: {
       target: "./src/index.ts",
       mode: "single",
-      client: "fetch",
+      client: "react-query",
+      httpClient: "fetch",
       prettier: false,
       clean: true,
+      override: {
+        operationName: (operation) => operation.operationId?.split("_api_")[0] ?? "unnamed_operation",
+        mutator: {
+          path: "./custom-fetch.ts",
+          name: "customFetch",
+        },
+        query: {
+          version: 5,
+        },
+        fetch: {
+          includeHttpResponseReturnType: false,
+          forceSuccessResponse: false,
+        },
+      },
     },
   },
 });
